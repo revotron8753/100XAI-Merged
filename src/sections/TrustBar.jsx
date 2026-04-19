86e2ea2bb71e4dd5742af4a7acb2030356d1b487
revotron8753 <piyushpandey98000@gmail.com>
2026-04-20 02:38:03 +0530

    bug fixes

--- src/sections/TrustBar.jsx
+++ src/sections/TrustBar.jsx
@@ -1,14 +1,14 @@
 import './TrustBar.css';
-import logo3 from '../assets/logos/3.svg';
-import logo4 from '../assets/logos/4.svg';
-import logo5 from '../assets/logos/5.svg';
-import logo6 from '../assets/logos/6.svg';
-import logo7 from '../assets/logos/7.svg';
-import logo8 from '../assets/logos/8.svg';
-import logo9 from '../assets/logos/9.svg';
-import logo10 from '../assets/logos/10.svg';
-import logo11 from '../assets/logos/11.svg';
-import logo12 from '../assets/logos/12.svg';
+import logo3 from '../assets/Logos/3.svg';
+import logo4 from '../assets/Logos/4.svg';
+import logo5 from '../assets/Logos/5.svg';
+import logo6 from '../assets/Logos/6.svg';
+import logo7 from '../assets/Logos/7.svg';
+import logo8 from '../assets/Logos/8.svg';
+import logo9 from '../assets/Logos/9.svg';
+import logo10 from '../assets/Logos/10.svg';
+import logo11 from '../assets/Logos/11.svg';
+import logo12 from '../assets/Logos/12.svg';
 
 const logos = [logo3, logo4, logo5, logo6, logo7, logo8, logo9, logo10, logo11, logo12];
 
